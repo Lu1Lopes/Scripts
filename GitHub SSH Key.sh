b3a78398ec5e87cd1c782088feb01abc769da154
Vincenzofdg <vfdgiacomo@gmail.com>
2022-02-10 09:38:45 -0300

    Git ssh

--- GitHub SSH Key.sh
+++ GitHub SSH Key.sh
@@ -6,7 +6,7 @@ read -p "Current web browser that you are using: " browser
 read -p "User name: " user
 read -p "Email: " email
 echo -e "\nVisual Studio Code 	==> 	code"
-echo -e "\nAtom 		   		==> 	atom\n"
+echo -e "\nAtom 		   	==> 	atom\n"
 read -p "Main Code Editor: " editor
 echo -e "\nUser: $user | Github Email: $email | Code Editor: $editor\n"
 read -p "All the information is correct? (y/n) " answer
@@ -14,7 +14,7 @@ if [ "y" = $answer ]; then
 	git config --global user.name $user
 	git config --global user.email $email
 	git config --global core.editor "$editor --wait"
-	code .gitconfig
+	#code .gitconfig
 	ssh-keygen -t rsa -b 4096 -C $email
 	eval "$(ssh-agent -s)"
 	ssh-add ~/.ssh/id_rsa
